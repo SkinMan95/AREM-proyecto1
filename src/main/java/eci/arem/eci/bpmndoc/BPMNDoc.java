f274858ffbdd917483cb4f5f995d3a978e4cd5f9
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-02-02 14:36:02 -0500

    commit inicial

---
+++ src/main/java/eci/arem/eci/bpmndoc/BPMNDoc.java
@@ -0,0 +1,16 @@
+package eci.arem.eci.bpmndoc;
+
+/**
+ *
+ * @author 2110137
+ */
+public class BPMNDoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Works");
+    }
+    
+}
